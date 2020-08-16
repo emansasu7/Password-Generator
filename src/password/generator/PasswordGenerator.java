862a62e8f32b0c4b8137662ece313d4e800ee51d
Emmanuel Sasu <eman.sasu7@gmail.com>
2020-08-16 16:54:59 +0200

    Add files via upload

---
+++ src/password/generator/PasswordGenerator.java
@@ -0,0 +1,10 @@
+package password.generator;
+
+public class PasswordGenerator {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new PGForm().setVisible(true);
+    }
+
+}
